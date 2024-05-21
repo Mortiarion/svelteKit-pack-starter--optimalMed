98964af67a01953005b92221cf8e63c2904713d8
Mood__Panda <andre-enju@ukr.net>
2024-05-21 21:31:47 +0300

    1. пофіксив шляхи хлібних крихт

--- src/routes/pages-doctors/alergologist/+page.svelte
+++ src/routes/pages-doctors/alergologist/+page.svelte
@@ -1,9 +1,9 @@
 <!-- <h1>ALERGOLOGIST</h1> -->
 
 <script lang="ts">
-	import Bredcrambs from '../base/components/Bredcrumbs.svelte';
+	import Bredcrambs from '$lib/components/components-pages-doctors/Bredcrumbs.svelte';
 	import type { BreadcrumbData } from '$lib/types.ts';
-    import SectionWork from '../base/components/SectionWork.svelte';
+    import SectionWork from '$lib/components/components-pages-doctors/SectionWork.svelte';
 
 	let BreadcrumbData: BreadcrumbData = {
 		bredCrumbsHead: 'Головна',
